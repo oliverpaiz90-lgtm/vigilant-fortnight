ec9a469596ff68d1fdbff89cfe0b32959da77812
Oliver <oliverpaiz90@gmail.com>
2026-09-25 07:13:48 -0600

    UI: Diseño responsive completo para dispositivos móviles

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors()); // habilitar CORS
 // Conexión a MySQL
